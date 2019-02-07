c15f93808ec711b2cf428a812a78ffc6c44c7bb4
dev4us <bronxofsoul@gmail.com>
2019-02-07 16:15:22 +0900

    Set graphQL Server and set up Middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,17 @@
-console.log("hey!");
+import app from "./app";
+import { Options } from "../node_modules/graphql-yoga";
+
+const PORT: number | string = process.env.port || 4000;
+const PLAYGROUND_ENDPOINT: string = "/playground";
+const GRAPHQL_ENDPOINT: string = "/graphql";
+
+const appOptions: Options = {
+  port: PORT,
+  playground: PLAYGROUND_ENDPOINT,
+  endpoint: GRAPHQL_ENDPOINT
+};
+const handleAppStart = () => {
+  console.log(`Listening on port ${PORT}`);
+};
+
+app.start(appOptions, handleAppStart);
